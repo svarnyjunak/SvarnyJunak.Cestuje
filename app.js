6eac42ff9e54277650a66c1cda711d19f290ba0a
svarnyjunak <svarnyjunak@gmail.com>
2015-12-25 19:20:44 +0100

    page 404 added

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var flickrOptions = {
-        api_key: process.env.API_KEY,
-        user_id: process.env.USER_ID     
-    };
+    api_key: process.env.API_KEY,
+    user_id: process.env.USER_ID
+};
 var flickr = require('./flickr');
-    
+
 var routes = require('./routes/index');
 var photoset = require('./routes/photoset');
 var users = require('./routes/users');
@@ -29,21 +29,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(function(req,res,next){
-  req.flickr = flickr;
-  req.flickrOptions = flickrOptions;
-  next();
+app.use(function (req, res, next) {
+    req.flickr = flickr;
+    req.flickrOptions = flickrOptions;
+    next();
 });
 
 app.use('/', routes);
 app.use('/photoset', photoset);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// catch 404 and show static page.
+app.use(function(req, res) {
+    console.log('xyz');
+    res.status(404);
+    res.sendFile(__dirname  + '/public/404.html');
 });
 
 // error handlers
@@ -51,24 +51,24 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    console.log('xyz');
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        console.log('xyz');
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
