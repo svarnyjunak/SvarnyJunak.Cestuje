ebf878e60e796aa779b80feab5c0e51fdfafaa8e
svarnyjunak <svarnyjunak@gmail.com>
2016-01-29 23:38:35 +0100

    gulp css minification added

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp'),
+    cssnano = require('gulp-cssnano'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     notify = require('gulp-notify');
 
-gulp.task('default', function() {
-  return gulp.src(['public/javascripts/lightbox.js'])
+gulp.task('styles', function() {
+  return gulp.src(['public/stylesheets/style.css'])
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(cssnano())
+    .pipe(gulp.dest('public/stylesheets/'))
+    .pipe(notify({ message: 'Styles minification complete' }));
+});
+
+gulp.task('scripts', function() {
+   return gulp.src(['public/javascripts/lightbox.js'])
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('public/javascripts/'))
-        .pipe(notify({ message: 'Javascript minification completed.' }));
+        .pipe(notify({ message: 'Javascript minification complete' }));  
+});
+
+gulp.task('default', function() {
+    gulp.start('styles', 'scripts');
 });
