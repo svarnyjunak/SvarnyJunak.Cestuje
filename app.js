a4f935ac155bc3fbf94f9747d4556bae5139b18b
svarnyjunak <svarnyjunak@gmail.com>
2016-01-31 15:24:19 +0100

    favicon added

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* global __dirname */
+/* global process */
 /* jshint node: true */
 
 var express = require('express');
@@ -19,12 +21,12 @@ var users = require('./routes/users');
 
 var app = express();
 
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
